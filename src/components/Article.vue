981f569574fee0f33a03ea767904027bfe3917a8
goonle <74893925+goonle@users.noreply.github.com>
2024-04-14 13:33:42 +0900

    v-for문을 통한 화면
    메인 화면을 위한 최소한의 레이아웃 작업

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="card d-flex col-xl-3 col-lg-4 col-md-4">
-        <div role="item-header">title</div>
-        <div role="item-body">body</div>
-        <div role="item-footer">footer</div>
+    <div class="d-flex col-xl-3 col-lg-4 col-md-4">
+        <div class="card my-2">
+            <div role="item-header">
+                <div class="float-start">O</div>
+                <div class="float-start">{{ item.keyword }}</div>
+            </div>
+            <img class="item-img" :src="item.src" src="">
+            <div role="item-body">
+                <p>{{ item.content }}</p>
+            </div>
+            <div role="item-footer">{{ item.reg_dt }}</div>
+        </div>
     </div>
 </template>
 <script>
+export default {
+    props: ['item']
+}
 </script>
 <style>
-.item-container {
-
+.item-img {
+    height: 150px;
+    border: 1px solid aqua;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,10 +2,15 @@
     <div class="container">
         <div class="row content-height" >
             <div class="left-list col-xl-3">
-                왼쪽
+                <ul>
+                    <li v-for="item in itemList">
+                        <div class="d-flex float-start" role="article keyword">{{item.keyword}}</div>
+                        <div class="d-flex float-end" role="article register date">{{item.reg_dt}}</div>
+                    </li>
+                </ul>
             </div>
             <div class="col-xl-9 right-content row">
-                <Item v-for="item in itemList">
+                <Item v-for="item in itemList" :item="item" :key="item.id">
                 </Item>
             </div>
         </div>
@@ -13,6 +18,15 @@
 </template>
 <script>
 import Item from './Article.vue';
+
+let itemList = [
+    { id: 1 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 1 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"},
+    { id: 2 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 2 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"},
+    { id: 3 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 3 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"},
+    { id: 4 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 4 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"},
+    { id: 5 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 5 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"},
+    { id: 6 , src: "/src/assets/logo_no_background.png", reg_dt : "2024-04-11", emotion_cd : 6 , keyword: "좋은 날이었다.", content : "dfawfsfd adfjahw edf jwaef sdkjfa s"}
+]
 
 export default {
     name: "Main",
@@ -22,7 +36,7 @@ export default {
     data() {
         return {
             content: "",
-            itemList: [1,2,3,4,5]
+            itemList: itemList
         }
     },
     mounted(){
