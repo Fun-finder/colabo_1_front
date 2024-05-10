acff6cc10ada82b1852a4bf891c88c044c1515ec
namdream <skaekqls789@naver.com>
2024-05-10 23:08:29 +0900

    기존 로그인 기능 오류 수정(profile화면이동까지)

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        followRedirects: true // 리디렉션된 요청에도 프록시를 적용
+      }
     }
   }
 })
