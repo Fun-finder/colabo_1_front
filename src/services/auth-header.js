179ce7bdf3423f306a94608398f47cbf2882dd8f
nam <skaekqls789@naver.com>
2024-05-15 08:13:05 +0900

    로그인 api

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,8 +1,22 @@
-export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken };
-    } else {
-        return {};
+import axios from 'axios';
+
+const API_URL = '/api/v1/user'; // 수정된 부분
+
+async function getCsrfToken() {
+    try {
+        const response = await axios.get(API_URL + '/csrf');
+        console.log('토큰: ', response.data.token)
+        return response.data.token;
+    } catch (error) {
+        throw error.response.data;
     }
-}
+};
+
+async function authHeader() {
+    const csrfToken = await getCsrfToken();
+    return {
+        'X-CSRF-TOKEN': csrfToken
+    };
+};
+
+export default authHeader;
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:9999',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // rewrite: (path) => path.replace(/^\/api/, ''),
         followRedirects: true // 리디렉션된 요청에도 프록시를 적용
       }
     }
